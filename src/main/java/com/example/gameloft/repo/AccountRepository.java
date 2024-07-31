cf9d765fa371a2e6e53f23c96495e8045c4af582
DESKTOP-KTN2TI9\Mihai <mihai@purerng.com>
2024-07-31 16:21:55 +0300

    cleanup and comments

--- src/main/java/com/example/gameloft/repo/AccountRepository.java
+++ src/main/java/com/example/gameloft/repo/AccountRepository.java
@@ -2,9 +2,11 @@ package com.example.gameloft.repo;
 
 import com.example.gameloft.model.Account;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<Account,Integer> {
-    Optional<Account> findById(Integer id);
+    @NonNull
+    Optional<Account> findById(@NonNull Integer id);
 }
--- src/main/java/com/example/gameloft/service/AccountService.java
+++ src/main/java/com/example/gameloft/service/AccountService.java
@@ -3,7 +3,6 @@ package com.example.gameloft.service;
 import com.example.gameloft.model.Account;
 import com.example.gameloft.model.Campaign;
 import com.example.gameloft.repo.AccountRepository;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -20,77 +19,83 @@ import java.util.concurrent.atomic.AtomicReference;
 public class AccountService {
     @Autowired
     AccountRepository accountRepository;
-    JsonParser p=null;
-    private ObjectMapper objectMapper=new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public Account findById(Integer id) {
-        Account account= accountRepository.findById(id).get();
-        List<Campaign> campaigns=CampaignService.getCurrentCampaigns();
-getInventory(account);
+
+        Account account = accountRepository.findById(id).orElseThrow();
+
+
+        List<Campaign> campaigns = CampaignService.getCurrentCampaigns();
+        //We can talk a while about how the inventory should be implemented.
+        try {
+            account.getInventory().putAll(objectMapper.readValue(account.getInventoryString(), new TypeReference<Map<String, Integer>>() {
+            }));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         for (Campaign campaign : campaigns) {
             if (isCompatible(account, campaign)) {
                 account.getActiveCampaigns().add(campaign);
             }
         }
-        try {
-            List<String> names=new ArrayList<>();
-            account.getActiveCampaigns().forEach(e->{
-               names.add(e.getName());
-            });
-          account.setActiveCampaignsString(objectMapper.writeValueAsString(names));
-          accountRepository.save(account);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        //I think this update should be somewhere else.
+        updateActiveCampaignForAccount(account);
         return account;
     }
 
-    void getInventory(Account account){
+    private void updateActiveCampaignForAccount(Account account) {
         try {
-            account.getInventory().putAll(objectMapper.readValue(account.getInventoryString(),new TypeReference<Map<String,Integer>>(){}));
-           /* JsonNode jsonNode= objectMapper.readTree(account.getInventoryString());
-            jsonNode.fields().forEachRemaining(e->{
-                account.getInventory().put(e.getKey(),e.getValue().asInt());
-            });*/
+            List<String> names = new ArrayList<>();
+            account.getActiveCampaigns().forEach(e -> {
+                names.add(e.getName());
+            });
+            account.setActiveCampaignsString(objectMapper.writeValueAsString(names));
+            accountRepository.save(account);
         } catch (JsonProcessingException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
-
     }
+
 
     private boolean isCompatible(Account account, Campaign campaign) {
         try {
             JsonNode jsonNode = objectMapper.readTree(campaign.getMatchers());
-            System.out.println(jsonNode.get("level").asText());
-            if(jsonNode.get("level").get("min").asInt()>account.getLevel())
+            /*-----------------------------------*/
+            if (jsonNode.get("level").get("min").asInt() > account.getLevel())
                 return false;
-            if(jsonNode.get("level").get("max").asInt()<account.getLevel())
+            if (jsonNode.get("level").get("max").asInt() < account.getLevel())
                 return false;
-            AtomicReference<Boolean> hasCountry= new AtomicReference<>(false);
-            jsonNode.get("has").get("country").elements().forEachRemaining(e->{
+            /*-----------------------------------*/
+            AtomicReference<Boolean> hasCountry = new AtomicReference<>(false);
+            jsonNode.get("has").get("country").elements().forEachRemaining(e -> {
                 hasCountry.set(hasCountry.get() ? hasCountry.get() : e.asText().equals(account.getCountry()));
             });
-            if(!hasCountry.get())
+
+            if (!hasCountry.get())
                 return false;
-            AtomicReference<Boolean> hasItem= new AtomicReference<>(true);
-            jsonNode.get("has").get("items").elements().forEachRemaining(e->{
+            /*-----------------------------------*/
+            AtomicReference<Boolean> hasItem = new AtomicReference<>(true);
+            jsonNode.get("has").get("items").elements().forEachRemaining(e -> {
                 hasItem.set(!hasItem.get() ? hasItem.get() : account.getInventory().containsKey(e.asText()));
             });
-            if(!hasItem.get())
+
+            if (!hasItem.get())
                 return false;
-            AtomicReference<Boolean> doesNotHaveItem= new AtomicReference<>(true);
-            jsonNode.get("does_not_have").get("items").elements().forEachRemaining(e->{
+            /*-----------------------------------*/
+            AtomicReference<Boolean> doesNotHaveItem = new AtomicReference<>(true);
+            jsonNode.get("does_not_have").get("items").elements().forEachRemaining(e -> {
                 doesNotHaveItem.set(!doesNotHaveItem.get() ? doesNotHaveItem.get() : !account.getInventory().containsKey(e.asText()));
             });
-            if(!doesNotHaveItem.get())
+
+            if (!doesNotHaveItem.get())
                 return false;
-        }catch (Exception e){
-
+            /*-----------------------------------*/
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-return true;
-    }
-
-    public List<Campaign> getCurrentCampaigns(){
-        return new ArrayList<>();
+        return true;
     }
 }
